f612701729b8ce60af1970300bb466c597b2fb07
Patrick Labatut <plabatut@meta.com>
2025-08-14 17:44:58 +0200

    Add missing header

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This software may be used and distributed in accordance with
+# the terms of the DINOv3 License Agreement.
+
 from dinov3.hub.backbones import (
     dinov3_convnext_base,
     dinov3_convnext_large,
